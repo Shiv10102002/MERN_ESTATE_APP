acf7693d40cbcf341948444795633855bc9700db
Shiv10102002 <shiv.20106107032@mitmuzaffarpur.org>
2024-04-05 12:57:36 +0530

    Add contact landlord functionality to the listing page

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { deleteUser, updateUser } from '../controllers/auth.controller.js';
+import {
+  deleteUser,
+  updateUser,
+  getUser,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get("/:id", verifyToken, getUser);
+
 
 export default router;
